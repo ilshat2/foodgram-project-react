d5b2b6628ac3e4ad8bf9ecf19a41ea956332b38b
Ильшат Кушманбетов <ilshat261093@yandex.ru>
2022-11-20 11:33:10 +0600

    * 1.4

---
+++ backend/api/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+
+from .models import (Favorite, Ingredient, Recipe,
+                     ShoppingList, Tag, IngredientInRecipe)
+from users.models import Follow
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_filter = ('author', 'name', 'tags')
+    list_display = ('name', 'followers')
+
+    @admin.display(empty_value=None)
+    def followers(self, obj):
+        return obj.favorite_recipes.all().count()
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_filter = ('name', )
+
+
+admin.site.register(Follow)
+admin.site.register(Tag)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Favorite)
+admin.site.register(ShoppingList)
+admin.site.register(IngredientInRecipe)
